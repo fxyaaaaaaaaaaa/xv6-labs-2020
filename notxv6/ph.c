28fb14626994e6e4d406746348e5634e69104e73
fxy <18431017619@163.com>
2025-03-05 16:24:31 +0800

    完成

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -4,10 +4,9 @@
 #include <assert.h>
 #include <pthread.h>
 #include <sys/time.h>
-
 #define NBUCKET 5
 #define NKEYS 100000
-
+pthread_mutex_t lock[NBUCKET];
 struct entry {
   int key;
   int value;
@@ -38,8 +37,9 @@ insert(int key, int value, struct entry **p, struct entry *n)
 static 
 void put(int key, int value)
 {
+      //获取锁
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,6 +53,8 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+
+  pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
@@ -102,7 +104,11 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
-
+  for(int i=0;i< NBUCKET;i++) //在执行put前初始化锁
+  {
+    pthread_mutex_init(&lock[i],NULL);
+  }
+  
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,37 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+//线程切换所需要的保存的寄存器
+struct context
+{
+    uint64 ra;
+    uint64 sp;
 
+
+    //  callee-saved
+    uint64 s0;
+    uint64 s1;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
+}; 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct  context context;      /* 在线程结构体中加入context结构体*/
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
-              
+extern void thread_switch(struct context*old, struct context*new);
+
 void 
 thread_init(void)
 {
@@ -63,6 +84,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->context,&next_thread->context);
   } else
     next_thread = 0;
 }
@@ -77,6 +99,11 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  // 返回地址，thread_switch线程切换执行完成后返回到ra，设置成线程函数func，就可以切换后执行func
+  t->context.ra = (uint64)func;
+
+  // 栈指针，将线程的栈指针指向其他独立的栈，栈的生长是从高地址到低地址，所以要将sp设置为指向stack的最高地址
+  t->context.sp = (uint64)&t->stack + (STACK_SIZE - 1);
 }
 
 void 
